403ae8c02cbc5b0353a409789bc1c1723c5acc3c
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-03 18:55:59 +0000

    port change

--- resturants/database_setup.py
+++ resturants/database_setup.py
@@ -26,5 +26,5 @@ class MenuItem(Base):
 	restaurant = relationship(Restaurant)
 
 
-engine = create_engine('postgresql://localhost:5432/restaurants')
+engine = create_engine('postgresql://localhost:5434/restaurants')
 Base.metadata.create_all(engine)
