bcdfbca29e9299f5ce50319273028e5133262be2
theydonthaveit <theydonthaveit@gmail.com>
2018-02-03 16:20:25 +0000

    new routes for new, edit and delete menu items

--- resturants/server.py
+++ resturants/server.py
@@ -1,28 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
-engine = create_engine('postgresql://localhost:5434/resturants')
+engine = create_engine('postgresql://localhost:5432/restaurants')
 Base.metadata.create_all(engine)
 
 DBsession = sessionmaker(bind=engine)
 session = DBsession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+def base():
+    return 'Hi'
 
-    output = ''
-    for i in items:
-        output += i.course
-        output += '</br>'
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
-    return output
+@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    return render_template('newMenuItem.html')
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return 'edit menu item'
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return 'delete menu item'
 
 if __name__ == '__main__':
     app.debug=True
