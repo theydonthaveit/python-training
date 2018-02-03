3119bd1fcebe4fe08adc115a14d2e10994d1ff84
Alan Williams <a.williams@mlwes51564.london.net-a-porter.com>
2018-02-03 10:50:50 +0000

    init setup for the db

---
+++ resturants/database_setup.py
@@ -0,0 +1,30 @@
+import sys
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+	__tablename__ = 'restaurant'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(80), nullable=False)
+
+
+class MenuItem(Base):
+	__tablename__ = 'menu_item'
+
+	id = Column(Integer, primary_key=True)
+	course = Column(String(250))
+	description = Column(String(250))
+	price = Column(String(8))
+	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+	restaurant = relationship(Restaurant)
+
+
+engine = create_engine('postgresql://localhost:5434/resturants')
+Base.metadata.create_all(engine)
